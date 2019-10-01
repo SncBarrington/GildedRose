7932e235eaada07ee8da096eb0c32e2541c14444
Valentin Zelionii <32639356+SncBarrington@users.noreply.github.com>
2019-10-01 17:06:33 +0200

    Unit Test Update 1
    Created first three basic Unit Tests:
    checkUsualUpdate() - checks normal degradation
    noNegativeQuality() - checks if quality is never negative
    doubleQualityDegradation() - checks once the sell by date has passed (<0), Quality degrades twice as fast

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,13 +7,42 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 1, 1) };
+    public void checkUsualUpdate() {
+        String name = "Dummie Core";
+        int n = 15;
+        Item[] items = new Item[] { new Item(name, n, n+10) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-        assertEquals(0,app.items[0].quality);
-        assertEquals(0,app.items[0].sellIn);
+        for (int i = n; i > 0; i--){
+            app.updateQuality();
+            assertEquals(name, app.items[0].name);
+            assertEquals(i-1,app.items[0].quality);
+            assertEquals(i-1,app.items[0].sellIn);
+        } 
+    }
+
+    @Test
+    public void noNegativeQuality() {
+        String name = "Crappy Dummie Core";
+        int n = 15;
+        Item[] items = new Item[] { new Item(name, n, n-5) };
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < n; i++){
+            app.updateQuality();
+            assertTrue(app.items[0].quality >= 0);
+        } 
+    }
+
+    @Test
+    public void doubleQualityDegradation() {
+        String name = "Old Dummie Core";
+        int n = 16;
+        Item[] items = new Item[] { new Item(name, 0, n) };
+        GildedRose app = new GildedRose(items);
+        for (int i = 1; i <= n; i++){
+            app.updateQuality();
+            assertEquals(n-i*2,app.items[0].quality);
+            assertEquals(-i,app.items[0].sellIn);
+        } 
     }
 
 }
