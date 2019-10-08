c0d4d9423a52e716c26754e81a9489b17490b43d
Valentin Zelionii <32639356+SncBarrington@users.noreply.github.com>
2019-10-08 16:19:05 +0200

    Update 2
    Added new unit tests for Aged Brie, Sulfuras and Backstage passes

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -26,7 +26,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item(name, n, j) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertTrue(app.items[0].quality == j);
+        assertEquals(j,app.items[0].quality);
     }
 
     @Test
@@ -38,6 +38,110 @@ public class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(j-2,app.items[0].quality);
+        assertEquals(i-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkAgedBrieNormal(){
+        String name = "Aged Brie";
+        int n = 10;
+        Item[] items = new Item[] { new Item(name, n, n) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(n+1,app.items[0].quality);
+        assertEquals(n-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkAgedBrieDouble(){
+        String name = "Aged Brie";
+        int i = 0;
+        int j = 10;
+        Item[] items = new Item[] { new Item(name, i, j) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(j+2,app.items[0].quality);
+        assertEquals(i-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkAgedBrieMaxQuality(){
+        String name = "Aged Brie";
+        int i = 10;
+        int j = 50;
+        Item[] items = new Item[] { new Item(name, i, j) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(j,app.items[0].quality);
+        assertEquals(i-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkSulfuras(){
+        String name = "Sulfuras, Hand of Ragnaros";
+        int i = 0;
+        int j = 80;
+        Item[] items = new Item[] { new Item(name, i, j) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(j,app.items[0].quality);
+        assertEquals(i,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkBackstagePassesNormal(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int n = 15;
+        Item[] items = new Item[] { new Item(name, n, n) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(n+1,app.items[0].quality);
+        assertEquals(n-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkBackstagePassesDouble(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int n = 10;
+        Item[] items = new Item[] { new Item(name, n, n) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(n+2,app.items[0].quality);
+        assertEquals(n-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkBackstagePassesTriple(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int n = 5;
+        Item[] items = new Item[] { new Item(name, n, n) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(n+3,app.items[0].quality);
+        assertEquals(n-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkBackstagePassesZero(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int i = 0;
+        int j = 25;
+        Item[] items = new Item[] { new Item(name, i, j) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].quality);
+        assertEquals(i-1,app.items[0].sellIn);
+    }
+
+    @Test
+    public void checkBackstageMaxQuality(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        int i = 5;
+        int j = 50;
+        Item[] items = new Item[] { new Item(name, i, j) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(j,app.items[0].quality);
         assertEquals(i-1,app.items[0].sellIn);
     }
 
