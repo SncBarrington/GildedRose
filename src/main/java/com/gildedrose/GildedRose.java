326e9477ef6550f84e40844c1be4c95ca27e11b3
Valentin Zelionii <32639356+SncBarrington@users.noreply.github.com>
2019-11-11 16:27:10 +0100

    Added constants
    Added new constants to GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,53 +1,62 @@
-package com.gildedrose;
+package main.java.com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+public class GildedRose {
+    
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGEPASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String BRIE = "Aged Brie";
+    
+    private Item[] items;
 
     public GildedRose(Item[] items) {
-        this.items = items;
+            this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11 && items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-
-                        if (items[i].sellIn < 6 && items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+            for (int i = 0; i < items.length; i++) {
+                if (!getItems()[i].name.equals(BRIE) && !getItems()[i].name.equals(BACKSTAGEPASS)) {
+                    if (getItems()[i].quality > 0 && !getItems()[i].name.equals(SULFURAS)) {
+                        getItems()[i].quality = getItems()[i].quality - 1;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (getItems()[i].quality < 50) {
+                        getItems()[i].quality = getItems()[i].quality + 1;
+
+                        if (getItems()[i].name.equals(BACKSTAGEPASS)) {
+                            if (getItems()[i].sellIn < 11 && getItems()[i].quality < 50) {
+                                getItems()[i].quality = getItems()[i].quality + 1;
+                            }
+
+                            if (getItems()[i].sellIn < 6 && getItems()[i].quality < 50) {
+                                getItems()[i].quality = getItems()[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+
+                if (!getItems()[i].name.equals(SULFURAS)) {
+                    getItems()[i].sellIn = getItems()[i].sellIn - 1;
+                }
+
+                if (getItems()[i].sellIn < 0) {
+                    if (!getItems()[i].name.equals(BRIE)) {
+                        if (!getItems()[i].name.equals(BACKSTAGEPASS)) {
+                            if (getItems()[i].quality > 0 && !getItems()[i].name.equals(SULFURAS)) {
+                                getItems()[i].quality = getItems()[i].quality - 1;
+                            }
+                        } else {
+                            getItems()[i].quality = 0;
+                        }
+                    } else {
+                        if (getItems()[i].quality < 50) {
+                            getItems()[i].quality = getItems()[i].quality + 1;
+                        }
                     }
                 }
             }
         }
+
+    public Item[]getItems(){
+        return this.items;
     }
 }
